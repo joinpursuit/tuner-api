769483dfcd3949b3243b78eb89e8521ee4c690d8
Ivan Castillo <total180training@gmail.com>
2021-07-04 02:16:50 -0400

    Set up pg-promise to facilitate connection between server and database

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,13 +1,12 @@
 //DEPENDENCIES
 const express = require("express")
 const songs = express.Router()
+const { getAllSongs } = require("../queries/songs")
 
 // INDEX
-songs.get("/", (req, res) => {
-    res.json({
-        status: "ok"
-    })
+songs.get("/", async (req, res) => {
+    const allSongs = await getAllSongs()
+    res.json(allSongs)
 })
-
 // EXPORTS
 module.exports = songs
