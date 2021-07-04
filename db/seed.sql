8113d9307a4fb00c340afe05662505588b3ec925
Ivan Castillo <total180training@gmail.com>
2021-07-04 00:14:27 -0400

    Set up .sql schema and seed files

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c songs_dev;
+
+INSERT INTO 
+songs (name, artist, album, time, is_favorite)
+VALUES
+('Don''t Go', 'Atom Force', 'After Dark', '3:55', true),
+('Confessions in the Dark', 'Swimware', 'Unknown', '4:00', true),
+('Firework', 'Katy Perry', 'Teenage Dream', '3:53', false);
