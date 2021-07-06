1b5c20e9637dffd76ef47c4234b34e6fd5b3ad4b
Alexander <alexanderl96@yahoo.com>
2021-07-06 12:40:40 -0400

    create show and read

--- controllers/songsControllers.js
+++ controllers/songsControllers.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../quries/songs");
+const { getAllSongs, getSongs, newSongs } = require("../quries/songs");
 
 songs.get("/", async (req, res)=>{
     const allSongs = await getAllSongs();
     res.json(allSongs);
+})
+
+songs.get("/:id", async (req,res)=>{
+    const { id} = req.params
+    try {
+        const oneSong = getSongs(id)
+        if(oneSong["id"]){
+            res.json(oneSong)
+        }else{
+            throw id
+        }
+    } catch (error) {
+        res.status(404).json({error: "Id not found", message: `This i an ${error}` })
+    }
+})
+songs.post("/", async (req,res)=>{
+    try {
+        const createSongs =newSongs(req.body)
+        if(createSongs["id"]){
+            res.json(createSongs)
+        }else{
+            throw req.body
+        }
+    } catch (error) {
+        res.status(404).json({error: "No new information has been add to DB ", message: `This post has end in a error ${error}` })
+    }
 })
 
 module.exports = songs;
--- quries/songs.js
+++ quries/songs.js
@@ -8,5 +8,23 @@ const getAllSongs = async ()=>{
         return err;
     }
 }
+const getSongs = async (id)=>{
+    try {
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id)
+        return oneSong
+    } catch (error) {
+        return error
+    }
+}
+const newSongs = async (song)=>{
+    try {
+        const createSongs = await db.one(
+        "INSERT INTO songs (name, artist, album, time, url, is_favorite) VALUES () RETURNING *", 
+        [song.name, song.artist, song.album, song.time, song.url, song.is_favorite])
+        return createSongs
+    } catch (error) {
+        return error
+    }
+}
 
-module.exports = { getAllSongs };
+module.exports = { getAllSongs, getSongs, newSongs };
