944f94f389e773269afc2b471841cc887f1ba841
Talia Pichardo <taliapichardo@gmail.com>
2021-07-07 03:44:40 -0400

    add seed

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Walking On A Dream', 'Empire Of the Sun', 'Walking on a Dream', '2008', true ),
+('Let it Happen', 'Tame Impala', 'Currents', '2015', true),
+('On the Low', 'Burna Boy', 'African Giant', '2019', true),
+('Eventually', 'Tame Impala', 'Currents', '2015', false),
+('Watermelon Sugar', 'Harry Styles', 'Fine Line', '2019', false),
+('So Will I', 'Hillsong Worship', 'There is More', '2018', true),
+('Toast', 'Koffee', 'Rapture', '2019', true ),
+('Love on the Brain', 'Rihanna', 'Anti', '2016', false),
+('Am I Wrong', 'Nico & Vinz', 'Absolute Music 73', '2013', true),
+('Million Little Miracles', 'Maverick City', 'Old Church Basement', '2021', true);
+
