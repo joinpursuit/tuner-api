7fcbd0d6d95ab9d0383383de6ca1e64a6f2e2ecf
Grace Bong <gracebong.park@gmail.com>
2021-07-06 22:22:58 -0400

    create and read one added

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const songs = express.Router();
-const { getSongs } = require('../queries/songs');
+const { getSongs, getOneSong } = require('../queries/songs');
 
 songs.get('/', async (req, res) => {
     const allSongs = await getSongs();
     res.json(allSongs);
 });
 
-// songs.get('/:id', async (req, res) => {
+songs.get('/:id', async (req, res) => {
+    const { id } = req.params;
 
-// });
+    try {
+        const song = await getOneSong(id);
 
-// songs.post();
+        if (song['id']) {
+            res.json(song);
+        } else {
+            throw `Database error: ${bookmark}`
+        };
+
+    } catch (err) {
+        res.status(404).json({
+            error: 'Not Found.',
+            messge: err
+        });
+    }
+});
+
+songs.post('/', async (req, res) => {
+    try {
+        const song = await newSong(req.body);
+
+        if (song['id']) {
+            res.json(song);
+        } else {
+            console.long(`Database error: ${song}`);
+            throw `Error adding ${req.body} to the database.`;
+        };
+
+    } catch (err) {
+        res.status(404).json({
+            error: err
+        });
+    };
+});
 
 // songs.put();
 
--- queries/songs.js
+++ queries/songs.js
@@ -9,6 +9,41 @@ const getSongs = async () => {
     }
 };
 
+const getOneSong = async (id) => {
+    try {
+        const oneSong = await db.one('SELECT * FROM songs WHERE id=$1', id);
+
+        if (oneSong[id]) {
+            return oneSong;
+        } else {
+            throw `Database error: ${oneSong}`
+        };
+
+    } catch (err) {
+        return err;
+    }
+};
+
+const newSong = async (song) => {
+    try {
+        const newSong = await db.one(
+            "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [
+                song.name,
+                song.artist,
+                song.album,
+                song.time,
+                song.is_favorite
+            ]
+        );
+        return newSong;
+    } catch (err) {
+        return err;
+    };
+};
+
 module.exports = {
     getSongs,
+    getOneSong,
+    newSong,
 };
