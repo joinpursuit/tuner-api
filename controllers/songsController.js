88d546848483c6df897d2325e50e9702a334c6d5
EsayH <ez@EsayHs-Mac-mini.local>
2021-07-06 21:46:22 -0400

    tuner-api pt 2

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,11 +1,42 @@
-const express = require('express');
-const songs = express.Router();
-const { getAllSongs } = require('../queries/songs');
+const express = require('express')
+const songs = express.Router()
+const { getAllSongs, getSong, createNewSong } = require('../queries/songs')
+log = console.log
 
-// Index
+// INDEX
 songs.get('/', async (req, res) => {
-  const allSongs = await getAllSongs();
-  res.json(allSongs);
+	const allSongs = await getAllSongs()
+	res.json(allSongs)
 })
 
-module.exports = songs;
+// SHOW
+songs.get('/:id', async (req, res) => {
+	const { id } = req.params
+	try {
+		const song = await getSong(id)
+		if (song['id']) {
+			res.json(song)
+		} else {
+			throw `No song found at index ${id}`
+		}
+	} catch (e) {
+		res.status(404).send({ error: 'Resource not found', message: e })
+	}
+})
+
+// CREATE
+songs.post('/', async (req, res) => {
+	try {
+		const song = await createNewSong(req.body)
+		if (song['id']) {
+			res.json(song)
+      console.log(song)
+		} else {
+			log(`Database: ${song}`)
+		}
+	} catch (e) {
+		res.status(404).json({ error: e })
+	}
+})
+
+module.exports = songs
--- queries/songs.js
+++ queries/songs.js
@@ -5,11 +5,33 @@ const getAllSongs = async () => {
   try {
     const allSongs = await db.any('SELECT * FROM songs');
     return allSongs
-  } catch (err) {
-    return err
+  } catch (e) {
+    return e
+  }
+};
+
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.one('SELECT * FROM songs WHERE id=$1', id)
+    return oneSong
+  } catch (e) {
+    return e
+  }
+};
+
+const createNewSong = async (song) => {
+  try {
+    const newSong = await db.one('INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *', 
+    [song.name, song.artist, song.album, song.time, song.is_favorite]);
+    return newSong
+  } catch (e) {
+    return e;
   }
 }
 
+
 module.exports = {
-  getAllSongs
-}
+  getAllSongs,
+  getSong,
+  createNewSong,
+};
