f5d499d42b93b076ce8b69464f48ebd6c8321afa
cassidybeni <cassidybeni@pursuit.org>
2021-07-07 00:36:01 -0400

    Complete part 2

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,14 +1,41 @@
-const express = requiree("express");
+const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
-songs.get("/", (req, res) => {
+songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
   res.json(allSongs);
 });
 
 songs.get("/", (req, res) => {
-  res.status(200);f
+  res.status(200);
+});
+
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const song = await getSong(id);
+    if (song.id) {
+      res.json(song);
+    } else {
+      throw `There is no song with id: ${id}`;
+    }
+  } catch (err) {
+    res.status(404).json({ error: "Song not found.", message: err });
+  }
+});
+
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    if (song.id) {
+      res.json(song);
+    } else {
+      throw `Error adding ${req.body} to the database.`;
+    }
+  } catch (err) {
+    res.status(404).json({ error: err });
+  }
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -1,14 +1,47 @@
 const db = require("../db/dbConfig");
 
 const getAllSongs = async () => {
-    try {
-        const allSongs = await db.any("SELECT * FROM songs");
-        return allSongs
-    } catch (error) {
-        return err
+  try {
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs;
+  } catch (err) {
+    return err;
+  }
+};
+
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return oneSong;
+  } catch (err) {
+    return err;
+  }
+};
+
+const createSong = async (song) => {
+  try {
+    if (!song.title) {
+      throw 'You must specify a value for "title"';
+    } else {
+      const newSong = await db.one(
+        "INSERT INTO songs (title, artist, album, song_length, is_favorite) VALUES( $1, $2, $3, $4, $5) RETURNING *",
+        [
+          song.title,
+          song.artist,
+          song.album,
+          song.song_length,
+          song.is_favorite,
+        ]
+      );
+      return newSong;
     }
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = {
-    getAllSongs,
+  getAllSongs,
+  getSong,
+  createSong,
 };
