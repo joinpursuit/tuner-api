dcf70d779c234d08c52769dfa5ab422941e2ca24
Hady Mohamed <hadymohamed@pursuit.org>
2021-07-07 00:51:53 -0400

    completed SHOW & CREATE and also set up some error logs in console as asked of the bonus

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
 // INDEX
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
   res.json(allSongs);
+});
+
+// SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const song = await getSong(id);
+    if (song["id"]) {
+      res.json(song);
+    } else {
+      console.log(`Database error: ${song}`);
+      throw `There is no bookmark with id: ${id}`;
+    }
+  } catch (e) {
+    res.status(404).json({ error: "Resource not found.", message: e });
+  }
+});
+
+// CREATE
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    if (song["id"]) {
+      res.json(song);
+    } else {
+      console.log(`Database error: ${song}`);
+      throw `Error adding ${req.body} to the database.`;
+    }
+  } catch (err) {
+    res.status(404).json({ error: err });
+  }
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -10,6 +10,49 @@ const getAllSongs = async () => {
   }
 };
 
+// SHOW
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return oneSong;
+  } catch (err) {
+    return err;
+  }
+};
+
+// CREATE
+const createSong = async (song) => {
+  try {
+    if (
+      !song.name ||
+      !song.artist ||
+      !song.album ||
+      !song.time ||
+      !song.is_favorite
+    ) {
+      throw "You must specify a value";
+    }
+    if (
+      typeof song.name !== "string" ||
+      typeof song.artist !== "string" ||
+      typeof song.album !== "string" ||
+      typeof song.time !== "string" ||
+      typeof song.is_favorite !== "boolean"
+    ) {
+      throw "You must specify a string or boolean";
+    }
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite],
+    );
+    return newSong;
+  } catch (err) {
+    return err;
+  }
+};
+
 module.exports = {
   getAllSongs,
+  getSong,
+  createSong,
 };
