8ba5464164a957e97ea2a81301824b956519b3d7
Ivan Castillo <total180training@gmail.com>
2021-07-08 03:42:45 -0400

    Set up Index, Show, Create, Delete routes

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,12 +1,66 @@
 //DEPENDENCIES
 const express = require("express")
 const songs = express.Router()
-const { getAllSongs } = require("../queries/songs")
+const { getAllSongs, getSong, createSong, deleteSong } = require("../queries/songs")
 
-// INDEX
+// Index
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs()
     res.json(allSongs)
 })
+
+// Show
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const bookmark = await getSong(id)
+        if (!bookmark.id) {
+            res.send("This bookmark id does not exist")
+        } else {
+            res.json(bookmark)
+        }
+    } catch (e) {
+        console.log(`Error: ${e}`)
+        res.json({ Error: `${e}`})
+    }
+})
+
+// Create
+songs.post("/", async (req, res) => {
+    try {
+        const newSong = await createSong(req.body)
+        if (newSong.id) {
+            res.json(newSong)
+        } else {
+            console.log(`Database Error: ${newSong}`)
+            throw `Error adding ${req.body} to the database`
+        }
+    } catch (e) {
+        console.log(`Error on create route: ${e}`)
+        res.status(404).json({ Error: e})
+    }
+})
+
+// Delete
+songs.delete("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedSong = await deleteSong(id)
+        if (deletedSong["name"]) {
+            res.status(200).json(deletedSong)
+        } else {
+            console.log("Error in Delete Route")
+            throw "Resource not found"
+        }
+    } catch (e) {
+        console.log(`Error in Delete Route - ${e}`)
+        res.json({Error: `Delete Route - ${e}`})
+    }
+})
+
+// Update
+
+
+
 // EXPORTS
 module.exports = songs
--- queries/songs.js
+++ queries/songs.js
@@ -1,5 +1,6 @@
 const db = require("../db/dbConfig.js")
 
+
 const getAllSongs = async () => {
     try {
         const allSongs = await db.any("SELECT * FROM songs")
@@ -9,5 +10,41 @@ const getAllSongs = async () => {
     }
 }
 
+const getSong = async (id) => {
+    try {
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id)
+        return oneSong
+    } catch (e) {
+        return e
+    }
+}
 
-module.exports = { getAllSongs }
+const createSong = async (song) => {
+    try {
+        if (!song.name) {
+            throw "You must specify a value for name"
+        } else {
+            const newSong = await db.one(
+                "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+                [song.name, song.artist, song.album, song.time, song.is_favorite]
+            )
+            return newSong
+        }
+    } catch (e) {
+        console.log(`Error in queries: ${e}`)
+        return e
+    }
+}
+
+const deleteSong = async (id) => {
+    try {
+        const deletedSong = await db.one("DELETE FROM songs WHERE id=$1 RETURNING *", id)
+        return deletedSong
+    } catch (e) {
+        console.log(`Error in queries`)
+        return e
+    }
+}
+
+
+module.exports = { getAllSongs, getSong, createSong, deleteSong }
