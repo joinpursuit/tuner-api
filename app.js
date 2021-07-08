432175154a382c615ac1d209a54c78a28eb644d1
Marsha Hall <mphall77@gmail.com>
2021-07-07 21:12:59 -0400

    add songs controller

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const songsController = require("./controllers/songsController");
 
 // CONFIGURATION
 const app = express();
@@ -10,8 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
+app.use("/songs", songsController);
 app.get("/", (req, res) => {
 	res.send("Welcome to Tuner App");
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+	res.status(404).send("Page not found");
 });
 
 // EXPORT
