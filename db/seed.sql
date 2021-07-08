b0c6e296309659a5100d6e602b4643fb50bb849e
Jordan Bobadilla <jordanboba08@gmail.com>
2021-07-08 18:11:33 -0400

    update db files

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Billie Jean', 'Michael Jackson', 'Thriller', '4:56', TRUE),
+('There Is Nothing Holding Me Back', 'Shawn Mendes', 'Illuminate', '3:20', FALSE),
+('Dont Stop Me Now', 'Queen', 'Jazz', '3:33', TRUE),
+('Uptown Funk', 'Mark Ronson Ft. Bruno Mars', 'Uptown Special', '4:31', TRUE)
+('Shake It Off', 'Taylor Swift', '1989', '3:39', FALSE);
