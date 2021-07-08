16db39fdefa5abc999a8c3ff4690d37e5581da6a
anjuhypp <anjuhypp@gmail.com>
2021-07-07 21:29:34 -0400

    setup db

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('bebaho', 'Yulduz Usmonova', 'bebaho', 2005, true),
+('My Love', 'Jill Scott', 'The Real Things Words and Sounds Vol 3', 2007, true)
