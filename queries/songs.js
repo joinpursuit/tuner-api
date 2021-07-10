003552bc577fc9a2a82838431a49e0c244bbd808
anjuhypp <anjuhypp@gmail.com>
2021-07-10 17:14:00 -0400

    clean code

--- queries/songs.js
+++ queries/songs.js
@@ -1,35 +1,35 @@
-const songs = require("../controllers/songsController.js")
-const db = require("../db/dbConfig.js")
+const songs = require("../controllers/songsController.js");
+const db = require("../db/dbConfig.js");
 
 const getAllSongs = async () => {
-    try {
-        const allSongs = await db.any("SELECT * FROM songs");
-        return allSongs;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getSong = async (id) => {
-    try {
-				const song = await db.one(`SELECT * FROM songs WHERE id = $1`, id)
-				return song
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const song = await db.one(`SELECT * FROM songs WHERE id = $1`, id);
+    return song;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const createSong = async (newSong) => {
-	const { name, artist, album, time, is_favorite } = newSong 
-    try {
-        const theSong = await db.one(
-					"INSERT INTO songs(name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
-					 [name, artist, album, time, is_favorite ]
-				)
-				return theSong
-    } catch (error) {
-			console.log(error)
-		}
+  const { name, artist, album, time, is_favorite } = newSong;
+  try {
+    const theSong = await db.one(
+      "INSERT INTO songs(name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [name, artist, album, time, is_favorite]
+    );
+    return theSong;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { getAllSongs, getSong, createSong };
