311af40566e799c659f85bf0315a44fd52290635
Carlo <carlonyc001@gmail.com>
2021-07-10 17:01:48 -0400

    show route complete

--- queries/songs.js
+++ queries/songs.js
@@ -1,12 +1,21 @@
 const db = require("../db/dbConfig");
 
-const getAllSongs = async () => {
-  try {
-    const allSongs = await db.any("SELECT * FROM songs");
-    return allSongs;
-  } catch (error) {
-    return error;
-  }
+const getAllSongs = async() => {
+    try {
+        const allSongs = await db.any("SELECT * FROM songs");
+        return allSongs;
+    } catch (error) {
+        return error;
+    }
+
 };
 
-module.exports = { getAllSongs };
+const getSongs = async(id) => {
+    try {
+        const song = await db.one("SELECT * FROM songs WHERE id=$1", id)
+    } catch (error) {
+        return error
+    }
+};
+
+module.exports = { getAllSongs, getSongs };
