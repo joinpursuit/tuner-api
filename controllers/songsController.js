471e0214f25c96e7023311a55b5a9e08ac1ea9aa
Miguel Garcia <miguelgarcia@pursuit.org>
2021-07-10 16:37:07 -0400

    added show route

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong } = require("../queries/songs");
 
 // INDEX
 songs.get("/", async (req, res) => {
-	const allSongs = await getAllSongs();
-	res.json(allSongs);
+  const allSongs = await getAllSongs();
+  res.json({ success: true, payload: allSongs });
+});
+
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const song = await getSong(id);
+    res.json({ success: true, payload: song });
+  } catch (err) {
+    return err;
+  }
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -1,12 +1,21 @@
 const db = require("../db/dbConfig.js");
 
 const getAllSongs = async () => {
-	try {
-		const allSongs = await db.any("SELECT * FROM songs");
-		return allSongs;
-	} catch (err) {
-		return err;
-	}
+  try {
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs;
+  } catch (err) {
+    return err;
+  }
 };
 
-module.exports = { getAllSongs };
+const getSong = async (id) => {
+  try {
+    const song = await db.one(`SELECT * FROM songs WHERE id = $1`, id);
+	return song;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = { getAllSongs, getSong };
