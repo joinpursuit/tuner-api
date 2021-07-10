38f91a45dc7557b32cc419033222f0e520196e65
joshuamendez <joshuafmendez@gmail.com>
2021-07-10 17:06:17 -0400

    part 1 with bonus completed

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,13 @@
--- INSERT INTO table VALUES()
+\c playlist
+
+INSERT INTO songs(
+    name,
+    artist,
+    album,
+    time,
+    is_favorite)     
+VALUES
+
+('Purple Rain','Prince','Pruple Rain','4:07',true),
+('In the End','Linkin Park','Hybrid Theory','3:36',true),
+('Chop Suey','System of a Down','Toxicity','3:30',false);
