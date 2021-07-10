9e99e8bda779709ffb26eb2c1b20e5bb85c9a0a9
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-09 23:16:22 -0400

    setup complete

--- db/seed.sql
+++ db/seed.sql
@@ -2,4 +2,7 @@
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('bebaho', 'Yulduz Usmonova', 'bebaho', 2005, true),
-('My Love', 'Jill Scott', 'The Real Things Words and Sounds Vol 3', 2007, true)
+('My Love', 'Jill Scott', 'The Real Things Words and Sounds Vol 3', 2007, true),
+('Can\t help falling in love', 'Elvis Presley', 'can\t help falling in love', 2012, true),
+('Table For One', 'Ego Ella May',  'Honey For Wounds', 2020, true),
+('Counting My Blessings', 'Nadia Batson', 'Get Soca 2021', 2021, true)
