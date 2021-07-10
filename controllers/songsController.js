65a4054b78cff3ff377777ff320b4d85ffe75a65
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-10 16:41:58 -0400

    part 2 done

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const songs = express.Router();
 
-const { 
-    getAllSongs, 
-    getSong, 
-    createSong 
-} = require("../queries/songs");
-
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
 songs.get("/", async (req, res) => {
-    const songs = await getAllSongs()
-    console.log("response", songs)
-    res.json({success: true, payload: songs})
-})
-
+  const songs = await getAllSongs();
+  console.log("response", songs);
+  res.json({ success: true, payload: songs });
+});
 
 songs.post("/", async (req, res) => {
-    const newSong = req.body;
-    const result = await createSong(newSong);
-    res.json(result);
-})
+  const newSong = req.body;
+  const result = await createSong(newSong);
+  res.json(result);
+});
 
 songs.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const song = await getSong(id);
-    res.json({success: true, payload: song});
-})
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.json({ success: true, payload: song });
+  } else {
+      res.redirect('/404')
+  }
+});
 
 module.exports = songs;
