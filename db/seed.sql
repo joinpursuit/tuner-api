599c0ef12d8b2976a004521da966d4c77f6d73c6
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-07-11 11:58:41 -0400

    finish first part of lab by connecting express to database

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Control', 'Janet Jackson', 'Best of Songs', '1980',true),
+('AY', 'Boogies', 'we Boogie collection', '1988', true),
+('DMV', 'Clarks', 'album of lines', '1976', true);
