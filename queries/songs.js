c2a24fcda5cfdcdd7a805b3305cfd07aab6e2a90
Jeffrey <jeffreysebastian@pursuit.org>
2021-07-11 07:48:37 -0700

    created a post

--- queries/songs.js
+++ queries/songs.js
@@ -21,7 +21,7 @@ const getSong = async (id) => {
 const createSong = async (song) => {
   try {
     const newSong = await db.one(
-      "INSERT INTO songs(name, artist, album, time, is_favorite)VALUES ($1, $2, $3) RETURNING *",
+      "INSERT INTO songs(name, artist, album, time, is_favorite)VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [song.name, song.artist, song.album, song.time, song.is_favosite]
     );
     return newSong;
