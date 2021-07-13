5bbb9b68b2ca68dab76b86630835088e36decb99
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-07-12 21:33:32 -0400

    create all routes

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  updateSong,
+  deleteSong,
+} = require("../queries/songs");
 
 songs.get("/", async (req, res) => {
   const songs = await getAllSongs();
-  res.json(songs);
+  res.json({success: true, payload: songs});
+});
+
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    if (song["id"]) {
+      res.json(song);
+    } else {
+      console.log(`database ${song}`);
+    }
+  } catch (e) {
+    res.status(404).json({ error: "Resource not found", message: e });
+  }
 });
 
 songs.get("/:id", async (req, res) => {
@@ -21,5 +40,25 @@ songs.get("/:id", async (req, res) => {
   }
 });
 
+songs.put("/:id", async (req, res) => {
+  const { body, params } = req;
+  const { name, artist, album, time } = body;
+  if (!name || !artist || !album || !time) {
+    res.status(422).json({
+      error: true,
+      success: false,
+      message: "did not update",
+    });
+  } else {
+    const updatedSong = await updateSong(params.id, body);
+    res.json(updatedSong);
+  }
+});
+
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  res.json({ success: true, payload: deletedSong });
+});
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -18,4 +18,45 @@ const getSong = async (id) => {
   }
 };
 
-module.exports = { getAllSongs, getSong };
+const createSong = async (song) => {
+  try {
+    const createdSong = await db.one(
+      "INSERT INTO songs(name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return createdSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateSong = async (id, song) => {
+  try {
+    const { name, artist, album, time, is_favorite } = song;
+    const query =
+      "UPDATE songs SET name = $1, artist = $2, album = $3, time = $4, is_favorite = $5 WHERE id = $6 RETURNING *";
+    const updatedSong = await db.one(query, [
+      name,
+      artist,
+      album,
+      time,
+      is_favorite,
+      id,
+    ]);
+    return updatedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteSong = async (id) => {
+  try {
+    const query = "DELETE FROM songs WHERE id = $1 RETURNING *";
+    const deletedSong = await db.one(query, id);
+    return deletedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllSongs, getSong, createSong, updateSong, deleteSong };
