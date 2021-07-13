2dd77a201e8168106ab20b548017b7ad41ee6b60
Shoaib Dar <SDAR30@gmail.com>
2021-07-12 22:02:33 -0400

    part 3 finished

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,9 +1,10 @@
-const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const { getAllSongs, getSong, createSong, deleteSong, updateSong } = require("../queries/songs");
 const songs = require("express").Router();
 
 
 songs.get("/", async (req, res) => {
-    const songs = await getAllSongs();
+    const  {order, is_favorite} = req.query;
+    const songs = await getAllSongs(order, is_favorite);
     res.json({ success: true, payload: songs })
 })
 
@@ -18,15 +19,33 @@ songs.get("/", async (req, res) => {
 // });
 
 songs.get("/:id", async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const song = await getSong(id);
-    song ? res.json({success: true, payload: song}) : res.redirect('/WrongID')
+    song ? res.json({ success: true, payload: song }) : res.redirect('/WrongID')
 })
 
-songs.post("/", async (req,res)=>{
+songs.post("/", async (req, res) => {
     const newSong = req.body;
     const song = await createSong(newSong);
-    song ? res.json({success: true, payload: song}) : res.redirect('/missingData')
+    song ? res.json({ success: true, payload: song }) : res.json({ success: false, error: "missing data" });
+})
+
+songs.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await deleteSong(id);
+    song ? res.json({ success: true, payload: song }) : res.json({ success: false, error: "wrong ID" });
+})
+
+songs.put("/:id", async (req, res) => {
+    const { body, params } = req;
+    const { name, artist, album, time, is_favorite } = req.body;
+    if (!name || !artist || !album || !time || typeof is_favorite !== 'boolean') {
+        res.status(422).json({ error: true, success: false, message: "you are missing some data" })
+    } else {
+        const result = await updateSong(params.id, body);
+        res.json({ success: true, payload: result });
+
+    }
 })
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -1,37 +1,72 @@
 const db = require("../db/config.js")
 console.log("--- inside queries/songs.js ---")
 
-const getAllSongs = async () => {
+const getAllSongs = async (order, is_favorite) => {
+    console.log(is_favorite)
+    let query = "SELECT * FROM songs"
+    if (is_favorite === 'true') {
+        query += " WHERE is_favorite = true"
+    } else if (is_favorite === 'false') {
+        query += " WHERE is_favorite = false"
+    }
+    if (order === 'asc') {
+        query += " ORDER BY name asc"
+    } else if (order === 'desc') {
+        query += " ORDER BY name desc"
+    }
+
     try {
-        //any, could be any number or rows if any
-        const allSongs = await db.any("SELECT * FROM songs")
+        const allSongs = await db.any(query)
+        console.log("running query:", query)
         return allSongs;
     } catch (error) {
         console.log(error)
     }
 }
 
-const getSong = async (id)=>{
-    try{
-        const song = await db.one(`SELECT * FROM songs WHERE id = $1`,id)
+const getSong = async (id) => {
+    try {
+        const song = await db.one(`SELECT * FROM songs WHERE id = $1`, id)
         return song;
-    }catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
 
-const createSong = async(newSong)=>{
-    const {name, artist, album, time, is_favorite} = newSong;
-    if(!name || !artist || !album || !time || !is_favorite){
+const createSong = async (newSong) => {
+    const { name, artist, album, time, is_favorite } = newSong;
+    if (!name || !artist || !album || !time || typeof is_favorite !== 'boolean') {
         return undefined;
     }
-    try{
-        const insertedSong = await db.one("INSERT INTO songs(name,artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *", [name, artist, album, time, is_favorite])
+    try {
+        const insertedSong = await db.one(
+            "INSERT INTO songs(name,artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [name, artist, album, time, is_favorite])
         return insertedSong;
-    }catch(error){
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const deleteSong = async (id) => {
+    try {
+        const song = await db.one(`DELETE FROM songs WHERE id = $1 RETURNING *`, id)
+        return song;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateSong = async (id, song) => {
+    try {
+        const { name, artist, album, time, is_favorite } = song
+        const query = "UPDATE songs SET name = $1, artist = $2, album = $3, time = $4, is_favorite = $5 WHERE id = $6 RETURNING *";
+        const updatedSong = await db.one(query, [name, artist, album, time, is_favorite, id])
+        return updatedSong;
+    } catch (error) {
         console.log(error)
     }
 }
 
 
-module.exports = {getAllSongs, getSong, createSong}
+module.exports = { getAllSongs, getSong, createSong, deleteSong, updateSong }
