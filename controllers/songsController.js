c5eccfd73b03281f3b8f2e5fd8969b081937f41f
Obaid Rustemi <orustemi@gmail.com>
2021-07-12 21:13:11 -0400

    add delete song functionality

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getSong,
+  newSong,
+  deleteSong,
+} = require("../queries/songs");
 
+// GET ALL SONGS
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
   res.json(allSongs);
 });
 
+// GET SPECIFIC SONG
 songs.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -21,7 +28,32 @@ songs.get("/:id", async (req, res) => {
     console.log(err);
     res.status(404).json({ error: "Resource not found", message: err });
   }
-  
+});
+
+// ADD A NEW SONG
+songs.post("/", async (req, res) => {
+  try {
+    const song = await newSong(req.body);
+    if (song["id"]) {
+      res.json(song);
+    } else {
+      console.log(`Database error: ${song}`);
+      throw `Error adding ${req.body} to the database`;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// DELETE A SONG
+songs.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const songToDelete = await deleteSong(id);
+    res.status(200).json(songToDelete);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -19,7 +19,42 @@ const getSong = async (id) => {
     return err;
   }
 };
+
+const newSong = async (song) => {
+  try {
+    const { name, artist, album, time, is_favorite } = song;
+    if (!name) {
+      throw `You must specify a value for 'Name'`;
+    }
+    const createNewSong = await db.one(
+      `INSERT INTO songs (
+    name,
+    artist,
+    album,
+    time,
+    is_favorite) 
+    VALUES ($1,$2,$3,$4,$5) RETURNING *`,
+      [name, artist, album, time, is_favorite]
+    );
+    return createNewSong;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteSong = async (id) => {
+  try {
+    const query = "DELETE FROM songs WHERE id = $1 RETURNING *";
+    const deletedSong = await db.one(query, id);
+    return deletedSong;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   getAllSongs,
   getSong,
+  newSong,
+  deleteSong,
 };
