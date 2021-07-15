3f67bb3d434e48656c780beb0219beffe37b8b53
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-14 21:05:47 -0400

    change time

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
-('Bebaho', 'Yulduz Usmonova', 'Bebaho', 2005, true),
-('My Love', 'Jill Scott', 'The Real Things Words and Sounds Vol 3', 2007, true),
-('Can\t help falling in love', 'Elvis Presley', 'can\t help falling in love', 2012, true),
-('Table For One', 'Ego Ella May',  'Honey For Wounds', 2020, true),
-('Counting My Blessings', 'Nadia Batson', 'Get Soca 2021', 2021, true);
+('Bebaho', 'Yulduz Usmonova', 'Bebaho', '2:05', true),
+('My Love', 'Jill Scott', 'The Real Things Words and Sounds Vol 3', '2:07', false),
+('Can\t help falling in love', 'Elvis Presley', 'can\t help falling in love', '2:12', true),
+('Table For One', 'Ego Ella May',  'Honey For Wounds', '2:20', true),
+('Counting My Blessings', 'Nadia Batson', 'Get Soca 2021', '2:21', true);
