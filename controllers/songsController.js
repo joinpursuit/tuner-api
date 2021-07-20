443c3342a1aaba24c84982bfca9bc8df758ea66e
Jamee Hengari <jhengari@hotmail.com>
2021-07-19 21:04:18 -0400

    third tuner api commit

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,7 +1,13 @@
 // Dependencies
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
 
 // Index
 songs.get("/", async (req, res) => {
@@ -12,14 +18,67 @@ songs.get("/", async (req, res) => {
   });
 });
 
+// Create
+songs.post("/add", async (req, res) => {
+  try {
+    const newSong = await createSong(req.body);
+    if (newSong["id"]) {
+      res.json({
+        success: true,
+        payload: newSong,
+      });
+    } else {
+      console.log(`Database error: ${newSong}`);
+      throw `Error adding ${req.body} to the database.`;
+    }
+  } catch (error) {
+    res.status(404).json({
+      error: "Song not added.",
+      message: error,
+    });
+  }
+});
+
 // Show
 songs.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const song = await getSong();
+    const song = await getSong(id);
+    if (song["id"]) {
+      res.json({
+        success: true,
+        payload: song,
+      });
+    } else {
+      console.log(`Database error: ${song}`);
+      throw `There is no song with the id: ${id}`;
+    }
   } catch (error) {
-    return error;
+    res.status(404).json({
+      error: "Resource not found.",
+      message: error,
+    });
   }
+});
+
+// Destroy
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  res.status(200).json({
+    success: true,
+    payload: deletedSong,
+  });
+});
+
+// Update
+songs.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  res.status(200).json({
+    success: true,
+    payload: updatedSong,
+  });
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -18,7 +18,49 @@ const getSong = async (id) => {
   }
 };
 
+const createSong = async (song) => {
+  try {
+    if (!song.name) {
+      throw 'You must specify a value "name"';
+    }
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, album, duration, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.duration, song.is_favorite]
+    );
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteSong = async (id) => {
+  try {
+    const deletedSong = await db.one(
+      "DELETE FROM songs WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateSong = async (id, song) => {
+  try {
+    const updatedSong = await db.one(
+      "UPDATE songs SET name=$1, artist=$2, album=$3, duration=$4, is_favorite=$5 WHERE id=$6 RETURNING *",
+      [song.name, song.artist, song.album, song.duration, song.is_favorite, id]
+    );
+    return updatedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
   getAllSongs,
   getSong,
+  createSong,
+  deleteSong,
+  updateSong,
 };
