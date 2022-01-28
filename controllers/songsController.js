fc56959f377301491454bed0d589aafc0493149c
salrey <tekdehn@gmail.com>
2022-01-28 18:40:02 -0500

    Setup songsController

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,6 +1,12 @@
+const { response } = require('express');
 const express = require('express');
 const songsRouter = express.Router();
+// const { getAllSongs } = require("../queries/songs")
 
+songsRouter.get('/', async (_, response) => {
+    const songs = await getAllSongs();
+    console.log("Get /songs");
+    response.status(200).json(songs);
+})
 
-
-module.exports = songs;
+module.exports = songsRouter;
