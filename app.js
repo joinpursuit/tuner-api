7d90fbc31118ffbaf444ea5511d1af1c4ce60fb6
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-28 17:31:52 -0500

    added songsController and added to app.js

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 const cors = require("cors");
 const express = require("express");
 
+// controllers
+const songsController = require("./controllers/songsController.js");
+
 // Configuration
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/songs", songsController);
 
 // Routes
 app.get("/", (req, res)=>{
-    res.status(200).send("Welcome to Tuner API App");
+    res.status(200).send("Welcome to Tuner API App!");
+})
+
+app.get("*", (req, res)=>{
+    res.status(404).send("404 - Page not found.");
 })
 
 // Export
