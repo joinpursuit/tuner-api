ab8bdb0d8320e42268d1df993d8bd0dc282c3ebb
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-28 18:16:52 -0500

    Stopping point for today

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+\c best_songs;
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
+('Freak on a Leash', "korn", "Follow the Leader", false), 
+(),
+(),
+(),
+("Zoo", "Taeyong, Jeno, Hendery, Yangyang, Gisells", "2021 Winter SMTOWN: SMCU EXPRESS", "2:57", TRUE), 
+("So What", "BTS", "LOVE YOURSELF 轉'TEAR', "4:40", TRUE);
