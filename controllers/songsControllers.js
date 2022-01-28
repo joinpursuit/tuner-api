e1d768b419abcbf0d7867e1f5fad7be90e62e9ff
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-28 17:47:38 -0500

    Created db folder with .js and .sql files and added sql command + copy paste Jose's commands from doc

---
+++ controllers/songsControllers.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const songs = express.Router();
+
+songs.get("/", (_, response) => {
+  response.json(songsArray);
+});
+
+songs.get("/:index", (request, response) => {
+  songsArray[request.params.index]
+    ? response.json(songsArray[request.params.index])
+    : response.status(404).json({ error: "Page not found" });
+});
+
+songs.post("/", (request, response) => {
+  songsArray.push(request.body);
+  response.json(songsArray);
+});
+
+songs.delete("/:index", (request, response) => {
+  if (songsArray[request.params.index]) {
+    const [deletedTransaction] = songsArray.splice(request.params.index, 1);
+    response.status(200).json(songsArray);
+  }
+});
+songs.put("/:index", (request, response) => {
+  songsArray[request.params.index] = request.body;
+  response.status(200).json(songsArray);
+});
+
+module.exports = songs;
