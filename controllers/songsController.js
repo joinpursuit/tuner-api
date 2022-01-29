c8f0c549aafd722a82eebf1e83b8ca08a3b2fed5
salrey <tekdehn@gmail.com>
2022-01-28 20:46:07 -0500

    GET one song, ADD new song

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,12 +1,30 @@
 const { response } = require('express');
 const express = require('express');
 const songsRouter = express.Router();
-const { getAllSongs } = require("../queries/songs")
+const { getAllSongs, getOneSong, addNewSong } = require("../queries/songs")
 
 songsRouter.get('/', async (_, response) => {
     const songs = await getAllSongs();
     console.log("Get /songs");
     response.status(200).json(songs);
+})
+
+songsRouter.get('/:index', async (request, response) => {
+    const { index } = request.params;
+    const song = await getOneSong(index);
+
+    console.log("Get /:index");
+    response.status(200).json(song);
+})
+
+songsRouter.post('/new', async (request, response) => {
+    const newSong = request.body;
+    const song = await addNewSong(newSong);
+
+    console.log(newSong)
+    console.log("CREATE new song");
+
+    response.status(201).json(song);
 })
 
 module.exports = songsRouter;
--- queries/songs.js
+++ queries/songs.js
@@ -10,6 +10,32 @@ const getAllSongs = async () => {
     }
 };
 
+const getOneSong = async (index) => {
+    try {
+        const song = await db.any(`SELECT * FROM songs WHERE id = ${index}`);
+        return song
+    } catch(err) {
+        return err
+    }
+};
+
+const addNewSong = async (newSong) => {
+    try {
+        const song = await db.any(
+            `INSERT INTO 
+                songs (name, artist, album, time, is_favorite)
+             VALUES
+                ('${newSong.name}', '${newSong.artist}', '${newSong.album}', '${newSong.time}', '${newSong.is_favorite}')
+            RETURNING *`
+        );
+        return song
+    } catch(err) {
+        return err
+    }
+};
+
 module.exports = {
     getAllSongs,
+    getOneSong,
+    addNewSong
 }
