afab99f94fc81f9bbfb6279df47c9f9ce58cd1d7
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-28 20:33:12 -0500

    creating connection with dbConfig.js

---
+++ db/dbConfig.js
@@ -0,0 +1,14 @@
+const pgp = require("pg-promise")();
+
+// connection object
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+};
+
+// open the connection. `cn` is short for `connection`
+const db = pgp(cn);
+
+module.exports = db;
