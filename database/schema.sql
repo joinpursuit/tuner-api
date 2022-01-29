e5d0b52ffd2afd1944d81841581bd5f5b937b6ff
justin <justin.d.diaz@gmail.com>
2022-01-29 16:39:02 -0500

    seed songs in

--- database/schema.sql
+++ database/schema.sql
@@ -4,4 +4,4 @@ CREATE DATABASE best_songs;
 
 \c best_songs;
 
-CREATE TABLE anime(id SERIAL PRIMARY KEY, name TEXT NOT NULL, arttist TEXT, album TEXT, time TEXT, is_favorite  BOOLEAN);
+CREATE TABLE songs(id SERIAL PRIMARY KEY, name TEXT NOT NULL, arttist TEXT, album TEXT, time TEXT, is_favorite  BOOLEAN);
