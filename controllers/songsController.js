d940e2ac8c7e1247b4c55ec2b07fefb32f80c6b2
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-28 20:49:23 -0500

    added getAllSongs query to songsController.

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const songs = express.Router();
+const { getAllSongs } = require("../queries/songs.js");
 
 // INDEX
+// new variable `allSongs` which is an array of songs objects. We have to await for the value to come back from the database.
 songs.get("/", async (req, res)=>{
-    res.json({ status: "ok" });
-})
+    const allSongs = await getAllSongs();
+    if (allSongs[0]) {
+        res.status(200).json(allSongs);
+    } else {
+        res.status(500).json({ error: "server error" });
+    }
+});
 
 module.exports = songs;
