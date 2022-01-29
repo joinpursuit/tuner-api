2491d4e52f3b460e559167ccc53d004379c43a2a
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-29 16:38:48 -0500

    Seed data for songs

--- database/seed.sql
+++ database/seed.sql
@@ -1,8 +1,8 @@
 \c best_songs;
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
-('Freak on a Leash', "korn", "Follow the Leader", false), 
-(),
-(),
-(),
-("Zoo", "Taeyong, Jeno, Hendery, Yangyang, Gisells", "2021 Winter SMTOWN: SMCU EXPRESS", "2:57", TRUE), 
-("So What", "BTS", "LOVE YOURSELF 轉'TEAR', "4:40", TRUE);
+('Freak on a Leash', 'korn', 'Follow the Leader', '4:27', false), 
+('Borderlines', 'Baroness', 'Gold & Grey', '6:17', false),
+('Making You Cry', ' A Boogie', 'B4AVA', '3:00', false),
+('Claro Cristal', 'Eladio Carrion', 'Sauce Boyz 2', '2:42', true),
+('Zoo', 'Taeyong, Jeno, Hendery, Yangyang, Gisells', '2021 Winter SMTOWN: SMCU EXPRESS', '2:57', true), 
+('So What', 'BTS', 'LOVE YOURSELF 轉 TEAR', '4:40', true);
