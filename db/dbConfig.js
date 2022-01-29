7532b36d0110036f01e9197dbcce626d00ba485e
wal3sca <walescaambroise@gmail.com>
2022-01-29 17:54:59 -0500

    configred database w pgpromise

---
+++ db/dbConfig.js
@@ -0,0 +1,12 @@
+const pgPromise = require("pg-promise");
+
+require("dotenv").config();
+
+const connection = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+};
+
+const database = pgPromise(connection);
