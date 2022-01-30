a90a3b6c6637eae7e48f22d6ab6db3c57715eab8
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-01-29 21:57:30 -0500

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Bad Habits', 'Ed Sheeran', 'Bad Habits', '2021', TRUE),
+('SHIVERS', 'Ed Sheeran', 'Shivers', '2021', TRUE),
+('Look What You Made Me Do', 'Taylor Swift', 'Reputation', '2017', TRUE ),
+('...Ready for It?', 'Taylor Swift', 'Reputation', '2017', TRUE);
