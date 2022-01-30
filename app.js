30367b57dd860d5826f3516c0f83b2063f822f51
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-01-30 15:59:46 -0500

    index route complete

---
+++ app.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require("express");
+const cors =require("cors");
+const songController = require("./controllers/songsController.js");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Welcome to Tuner App");
+});
+
+app.use("/songs", songController)
+
+// 404 PAGE
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
+('Put Your Head On My Shoulder', 'Paul Anka', 'Sings His Favorites', '2:35', true),
+('You', 'Galantis', 'Pharmacy', '3:35', true),
+('One More Time', 'Daft Punk', 'Discovery', '5:20', true),
+('Over The Top', 'Smiley', 'Buy or Bye 2', '2:33', false),
+('Smooth Criminal', 'Michael Jackson', 'bad', '4:17', true),
+('Gucci Gang', 'Lil Pump', 'Lil Pump', '2:04', false),
+('No Role Models', 'J. Cole', '2014 Forest Hills Drive', '4:52', true);
