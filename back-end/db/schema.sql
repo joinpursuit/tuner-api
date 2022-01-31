d09624fcd442d6866c073857299f7ac380a8d8d0
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-01-31 01:11:20 -0500

    update schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE songs (
     name TEXT NOT NULL,
     artist TEXT,
     album TEXT,
-    time TEXT,
+    time NUMBER,
     is_favorite BOOLEAN
 );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,6 +3,6 @@
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Love Song', 'The Cure', 'Disintegration', 1989, true),
 ('Break it off', 'Pink Pantheress', 'To hell with it', 2021, true),
-('(Fallin like) Dominoes', 'Donald Byrd', 'Places and Spaces ', 1975, true),
+('(Fallin like) Dominoes', 'Donald Byrd', 'Places and Spaces', 1975, true),
 ('Rhymes like Dimes', 'MF DOOM', 'Operation: Doomsday', 1999, true),
 ('Soul', 'soLUMINATTI', 'Rebirth', 2022, false);
