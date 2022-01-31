7f8aa2dac02c21114917c82e938b4ab68e2ad47b
Triane Peart <TrianePeart@gmail.com>
2022-01-31 10:40:46 -0500

    created a get song

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const songs = express.Router();
-const {getAllSongs} = require('../queries/songs');
+const {getAllSongs, getSong} = require('../queries/songs');
+
 
 songs.get('/', async (req, res) => {
     const allSongs = await getAllSongs();
     console.log(allSongs)
     res.status(200).json(allSongs)
 });
+
+songs.get("/:id", async (req, res) =>{
+    const {id} = req.params
+    const song = await getSong (id);
+    if (song) {
+        res.status(200).json(song)
+    }else{
+        res.status(404).json({ Error: `Page not found!` });
+    }
+})
 
 module.exports = songs; 
