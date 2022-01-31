8e87acbfeccb85447c659e4a55d296feb5e5ebd4
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-31 02:09:48 -0500

    troubleshooting error:  option 'host' is not recognized

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const songs = express.Router();
+const { getAllSongs } = require("../queries/songs.js");
 
 //Index
-songs.get("/", (req, res) => {
-    res.status(202).json( {message: "All good!"} );
+songs.get("/", async (req, res) => {
+    const allSongs = await getAllSongs();
+    if(allSongs[0]){
+        res.status(200).json(allSongs);
+    }else {
+        res.status(500).json({ error: "server error" });
+    }
 })
 
 module.exports = songs;
-
--- queries/songs.js
+++ queries/songs.js
@@ -1,1 +1,15 @@
 //requests to access data from a database to manipulate it or retrieve it
+const db = require("../db/dbConfig");
+
+const getAllSongs = async () => {
+    try{
+        const allSongs = await db.any("SELECT * FROM songs;");
+        return allSongs;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getAllSongs
+}
