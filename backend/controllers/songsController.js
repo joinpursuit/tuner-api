882bbdd8c0f9f1a430c7cc668ebaed0fd18c12d7
Jerry De Jesus <jdejesus7@live.com>
2022-02-01 16:36:10 -0500

    index route complete

--- backend/controllers/songsController.js
+++ backend/controllers/songsController.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const songs = express.Router();
+const { getAllSongs } = require('../queries/songs');
 
 songs.get("/", async (req, res) => {
-  res.json({ status: "ok" });
+  const allSongs = await getAllSongs();
+  if(allSongs[0]){
+    res.status(200).json(allSongs);
+  }else{
+    res.status(500).json({ error: "server error"});
+  }
 });
+
+
 
 module.exports = songs;
