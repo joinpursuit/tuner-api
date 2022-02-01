b42568d5172cef15022084f419ff1d7434c4223b
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-02-01 14:56:30 -0500

    Added get one song functionality

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getAllSongs } = require("../queries/songs");
-
+const { getAllSongs, oneSong } = require("../queries/songs");
+//get all songs
 router.get("/", async (req, res) => {
   const songs = await getAllSongs();
   res.json(songs);
+});
+//get one song
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await oneSong(id);
+
+  if (song.id) res.json(song);
+  else res.redirect("/*");
 });
 
 module.exports = router;
--- queries/songs.js
+++ queries/songs.js
@@ -8,5 +8,13 @@ const getAllSongs = async () => {
     return error;
   }
 };
+const oneSong = async (id) => {
+  try {
+    const song = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return song;
+  } catch (error) {
+    return error;
+  }
+};
 
-module.exports = { getAllSongs };
+module.exports = { getAllSongs, oneSong };
