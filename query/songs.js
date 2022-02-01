cfd30865821bbe6059d68eae5e534042e9f6b452
Rohan-Sterling <rohansterling@pursuit.org>
2022-02-01 18:22:27 -0500

    add function to get one song and function to add one song to database

--- query/songs.js
+++ query/songs.js
@@ -10,4 +10,25 @@ const getAllSongs = async () => {
     }
 }
 
+export const getSong = async (id) => {
+    try {
+        const song = await db.one("SELECT * FROM songs WHERE id=$1", id)
+        return song
+    }
+    catch(err){
+        return err
+    }
+}
+
+export const createSong = async ({name, artist, album, time, is_favorite}) => {
+    try {
+        const song = await db.one("INSERT INTO songs(name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *", [name, artist, album, time, is_favorite]);
+    return song
+    }
+    catch(err){
+        return err
+    }
+    
+}
+
 export default getAllSongs
