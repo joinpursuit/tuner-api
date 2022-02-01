231d401239c595d00481b6af06acfe61f3e9180d
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 15:16:32 -0500

    added if/else in try/catch for CREATE in songsController

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -36,8 +36,12 @@ songs.get("/:id", async (req, res)=>{
 // CREATE
 songs.post("/", async (req, res)=>{
     try {
-        const song = await createSong(req.body);
-        res.status(200).json(song);
+        const createdSong = await createSong(req.body);
+        if (createdSong.id) {
+            res.status(200).json(createdSong);
+        } else {
+            res.status(500).json({ error: "Song creation error. Missing field" })
+        }
     } catch (error) {
         res.status(400).json({ error: "Song was not created!" })
     }
