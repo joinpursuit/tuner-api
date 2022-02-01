1f0634abe3f5833365f51e311d120e93b683e147
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 15:04:19 -0500

    added createSong query in songs.js

--- queries/songs.js
+++ queries/songs.js
@@ -22,8 +22,22 @@ const getSong = async (id) => {
     }
 }
 
+/* CREATE */
+const createSong = async (song) => {
+    try {
+        const newSong = await db.one(
+            "INSERT INTO songs (name, album, time, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
+            [song.name, song.album, song.time, song.is_favorite]
+        );
+        return newSong;
+    } catch (error) {
+        return error;
+    }
+}
+
 // returning an object because we are going to return more than one function
 module.exports = {
     getAllSongs,
     getSong,
+    createSong,
 };
