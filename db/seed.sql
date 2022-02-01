9055e90291e47cd19ab6f3c8f3bea3a2fcc4a71c
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-01 15:22:54 -0500

    create an async arrow function

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,7 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Perfect', 'Ed Sheeran', 'Perfect', '2017', TRUE),
 ('Bad Habits', 'Ed Sheeran', 'Bad Habits', '2021', TRUE),
 ('SHIVERS', 'Ed Sheeran', 'Shivers', '2021', TRUE),
 ('Look What You Made Me Do', 'Taylor Swift', 'Reputation', '2017', TRUE ),
--- queries/songs.js
+++ queries/songs.js
@@ -9,4 +9,15 @@ const getAllSongs = async () => {
   }
 };
 
+// ONE Song
+const getSong = async () => {
+  try {
+    const oneSong = await db.one("SELECT * FROM song WHERE id=$1", id);
+    return oneSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+
 module.exports = { getAllSongs };
