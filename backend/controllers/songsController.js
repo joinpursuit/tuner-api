159f819ebda746e0df7528979647248c82160d42
JoshMarte <joshuelm@gmail.com>
2022-02-01 11:31:57 -0500

    some validation

--- backend/controllers/songsController.js
+++ backend/controllers/songsController.js
@@ -1,7 +1,19 @@
 // DEPENDENCIES
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../../queries/songs");
+const { checkName, checkBoolean } = require("../../validations/checkSongs");
+
+// SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -10,6 +22,16 @@ songs.get("/", async (req, res) => {
     res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// CREATE
+songs.post("/", checkBoolean, checkName, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
--- queries/songs.js
+++ queries/songs.js
@@ -1,5 +1,6 @@
 const db = require("../db/dbConfig");
 
+// ALL SONGS
 const getAllSongs = async () => {
   try {
     const allSongs = await db.any("SELECT * FROM songs");
@@ -9,4 +10,36 @@ const getAllSongs = async () => {
   }
 };
 
-module.exports = { getAllSongs };
+// ONE SONG
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+    return oneSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+// await db.one("SELECT * FROM songs WHERE id=$[id]", {
+//   id: id,
+// });
+
+// CREATE
+const createSong = async (song) => {
+  try {
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+//EXPORT
+module.exports = {
+  getAllSongs,
+  getSong,
+  createSong,
+};
