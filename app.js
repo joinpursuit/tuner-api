29849385674cd1c75194d02ac87170bb2d91b322
hinkabi <hinkabi@gmail.com>
2022-01-31 23:25:43 -0500

    Working homepage need to set up database

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const songsController = require("./controllers/songsController.test.js");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+//ROUTES
+app.get("/" ,(req,res) =>{
+    res.send("welcome to our Music App");
+});
+
+app.use("/songs", songsController);
+
+app.get("*", (req,res) =>{
+    res.status(404).send("Page not found")
+});
+
+module.exports = app;
