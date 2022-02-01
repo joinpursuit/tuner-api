566ee3d52acf536426b24196d3c44c38f02745bc
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 15:05:00 -0500

    added route with createSong query to songsController for CREATE

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const songs = express.Router();
 // import queries
-const { getAllSongs, getSong } = require("../queries/songs.js");
+const { getAllSongs, getSong, createSong } = require("../queries/songs.js");
 
 // INDEX
 // new variable `allSongs` which is an array of songs objects. We have to await for the value to come back from the database.
@@ -30,6 +30,16 @@ songs.get("/:id", async (req, res)=>{
         }
     } catch (err) {
         console.log(err);
+    }
+})
+
+// CREATE
+songs.post("/", async (req, res)=>{
+    try {
+        const song = await createSong(req.body);
+        res.status(200).json(song);
+    } catch (error) {
+        res.status(400).json({ error: "Song was not created!" })
     }
 })
 
