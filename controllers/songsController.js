91a9fdf8c523397858f71d620e85189c322e6cf6
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 14:33:22 -0500

    put INDEX and SHOW songsControllers into try/catch statements. testing SHOW in postman successful

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js");
+// import queries
+const { getAllSongs, getSong } = require("../queries/songs.js");
 
 // INDEX
 // new variable `allSongs` which is an array of songs objects. We have to await for the value to come back from the database.
 songs.get("/", async (req, res)=>{
-    const allSongs = await getAllSongs();
-    if (allSongs[0]) {
-        res.status(200).json(allSongs);
-    } else {
-        res.status(500).json({ error: "server error" });
+    try {
+        const allSongs = await getAllSongs();
+        if (allSongs[0]) {
+            res.status(200).json(allSongs);
+        } else {
+            res.status(500).json({ error: "server error" });
+        }
+    } catch (err) {
+        console.log(err);
     }
 });
+
+// SHOW
+songs.get("/:id", async (req, res)=>{
+    const { id } = req.params;
+    try {
+        res.json({ id }); // testing in postman, it works
+        
+    } catch (err) {
+        console.log(err);
+    }
+})
 
 module.exports = songs;
