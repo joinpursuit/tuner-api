f9a3cd1eae98a40f3703d367a5a271f0a2542f8c
Rohan-Sterling <rohansterling@pursuit.org>
2022-02-01 16:56:26 -0500

    import songsController

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from "express"
 import cors from "cors"
+import songsController from "./controllers/songsController.js"
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use('/song', songsController)
+
 app.get("/", (request, response) => {
-    response.send("Welcome")
+    response.send("Welcome to Tuner")
+})
+
+app.get("*", (request, response) => {
+    response.send("Error")
 })
 
 export default app
