b805c1ec2279cce14bf488f8920bdc6928cbdc66
Name Ping Louie <namepinglouie@gmail.com>
2022-02-02 12:19:46 -0500

    completed part 3

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const songs = express.Router();
 
-const {getAllSongs, getSong, createSong} = require("../queries/songs.js");
+const {getAllSongs, getSong, createSong, deleteSong, updateSong} = require("../queries/songs.js");
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
@@ -13,7 +13,7 @@ songs.get("/:id", async (req, res) => {
     const {id} = req.params;
     const song = await getSong(id);
     if(song.id) res.status(200).json(song);
-    else res.status(500).json({error: "Song not found"});
+    else res.status(404).json({error: "Song not found"});
 });
 
 songs.post("/", async (req, res) => {
@@ -21,6 +21,21 @@ songs.post("/", async (req, res) => {
     const createdSong = await createSong(body);
     if(createdSong.id) res.status(200).json(createdSong);
     else res.status(500).json({error: "Song creation error"});
+});
+
+songs.delete("/:id", async (req, res) => {
+    const {id} = req.params;
+    const deletedSong = await deleteSong(id);
+    if(deletedSong.id) res.status(200).json(deletedSong);
+    else res.status(404).json({error: "Song not found"});
+});
+
+songs.put("/:id", async (req, res) => {
+    const {id} = req.params;
+    const {body} = req;
+    const updatedSong = await updateSong(id, body);
+    if(updatedSong.id) res.status(200).json(updatedSong);
+    else res.status(404).json({error: "Song not found"});
 });
 
 module.exports = songs;
