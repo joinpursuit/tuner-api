0c4c80287212a772e1da5ff31186b369866ee160
Name Ping Louie <namepinglouie@gmail.com>
2022-02-02 11:56:48 -0500

    completed part 2

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const songs = express.Router();
 
-const {getAllSongs} = require("../queries/songs.js");
+const {getAllSongs, getSong, createSong} = require("../queries/songs.js");
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
     if(allSongs[0]) res.status(200).json(allSongs);
     else res.status(500).json({error: "server error"});
+});
+
+songs.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    const song = await getSong(id);
+    if(song.id) res.status(200).json(song);
+    else res.status(500).json({error: "Song not found"});
+});
+
+songs.post("/", async (req, res) => {
+    const {body} = req;
+    const createdSong = await createSong(body);
+    if(createdSong.id) res.status(200).json(createdSong);
+    else res.status(500).json({error: "Song creation error"});
 });
 
 module.exports = songs;
--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE db_songs;
 \c db_songs;
 
 CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,
     name TEXT,
     artist TEXT,
     album TEXT,
