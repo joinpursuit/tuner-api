4b0da51cfed944ba8ae8356c8e6474451ef7c6fb
Oliver Chuzan <ochuzan@gmail.com>
2022-02-01 20:48:07 -0500

    Part 2 Complete w/ Bonus/Validations

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,13 +1,48 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js");
+const { getAllSongs, getSong, createNewSong } = require("../queries/songs.js");
+const { checkName, checkArtist, checkAlbum, checkTime, checkIsFavorite } = require("../validations/checkSong.js");
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
-    if(allSongs[0]){
-        res.status(200).json(allSongs);
-    } else {
-        res.status(500).json({error: "Server Error"});
+    try {
+        if(allSongs[0]){
+            res.status(200).json(allSongs);
+        } else {
+            res.status(500).json({error: "Server Error"});
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const song = await getSong(id);
+        if(song.id){
+            res.status(200).json(song);
+        } else {
+            res.status(500).redirect("*");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+songs.post("/", checkName, checkArtist, checkAlbum, checkTime, checkIsFavorite, async (req, res) => {
+    const { body } = req;
+
+    try {
+        const newSong = await createNewSong(body);
+        if(newSong.id){
+            res.status(200).json(newSong);
+        } else {
+            res.status(500).json({error: "Song creation error"});
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
 
---
+++ validations/checkSong.js
@@ -0,0 +1,49 @@
+const checkName = (req, res, next) => {
+    if(req.body.name){
+        next();
+    } else {
+        res.status(400).json({error: "Name is required"});
+    }
+};
+
+const checkArtist = (req, res, next) => {
+    if(req.body.artist){
+        next();
+    } else {
+        res.status(400).json({error: "Artist is required"});
+    }
+};
+
+const checkAlbum = (req, res, next) => {
+    if(req.body.album){
+        next();
+    } else {
+        res.status(400).json({error: "Album is required"});
+    }
+};
+
+const checkTime = (req, res, next) => {
+    let regCheck = req.body.time.match(/^\d:\d\d/g);
+    if(regCheck){
+        next();
+    } else {
+        res.status(400).json({error: "Time is required in following format 1:23"});
+    }
+};
+
+const checkIsFavorite = (req, res, next) => {
+    
+    if(typeof req.body.is_favorite === "boolean"){
+        next();
+    } else {
+        res.status(400).json({error: "Is Favorite is required"});
+    }
+};
+
+module.exports = {
+    checkName,
+    checkArtist,
+    checkAlbum,
+    checkTime,
+    checkIsFavorite
+}
