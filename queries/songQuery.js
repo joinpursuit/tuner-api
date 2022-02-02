3f57279ed3998d58ae7c382a21005a8df698be57
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-02 10:30:39 -0500

    updated how vars show in query 4 security reasons

--- queries/songQuery.js
+++ queries/songQuery.js
@@ -16,9 +16,9 @@ const getAllSongs = async () => {
 
 const getSongById = async (id) => {
     try {
-        const theSong = await database.any(`
-            SELECT * FROM songs_tb WHERE id = ${id} ;
-        `);
+        const theSong = await database.one(`
+            SELECT * FROM songs_tb WHERE id=$1 ;
+        `, id);
         return theSong;
     } catch(error) {
         return error;
@@ -27,13 +27,13 @@ const getSongById = async (id) => {
 
 const postAndGet = async ({ title, artist, album, length, is_favorite }) => {
     try {
-        const postedSong = await database.any(`
+        const postedSong = await database.one(`
             INSERT INTO songs_tb
                 (title, artist, album, length, is_favorite) 
             VALUES
-                ('${title}', '${artist}', '${album}', '${length}', ${is_favorite})
+                ($1, $2, $3, $4, $5)
             RETURNING * ;
-        `);
+        `, title, artist, album, length, is_favorite);
         return postedSong;
     } catch(error) {
         return error;
@@ -42,11 +42,11 @@ const postAndGet = async ({ title, artist, album, length, is_favorite }) => {
 
 const deleteAndGet = async (id) => {
     try {
-        const deletedSong = await database.any(`
+        const deletedSong = await database.one(`
             DELETE FROM songs_tb
-            WHERE id = ${id} 
+            WHERE id=$1
             RETURNING * ;
-        `);
+        `, id);
         return deletedSong;
     } catch(error) {
         return error;
@@ -55,16 +55,16 @@ const deleteAndGet = async (id) => {
 
 const updateAndGet = async (id, { title, artist, album, length, is_favorite }) => {
     try {
-        const updatedSong = await database.any(`
+        const updatedSong = await database.one(`
             UPDATE songs_tb 
             SET 
-                title = '${title}', 
-                artist = '${artist}', 
-                album = '${album}', 
-                length = '${length}', 
-                is_favorite = ${is_favorite} 
-            WHERE id = ${id} RETURNING * ; 
-        `);
+                title = $1, 
+                artist = $2, 
+                album = $3, 
+                length = $4, 
+                is_favorite = $5 
+            WHERE id = $6 RETURNING * ; 
+        `, title, artist, album, length, is_favorite, id);
         return updatedSong;
     } catch(error) {
         return error;
