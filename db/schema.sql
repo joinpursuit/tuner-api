bb15927c76693f6aa69be8917d7342600db32f4d
Rohan-Sterling <rohansterling@pursuit.org>
2022-02-01 20:28:41 -0500

    add uuid to id

--- db/schema.sql
+++ db/schema.sql
@@ -4,4 +4,4 @@ CREATE DATABASE my_playlist;
 
 \c my_playlist
 
-CREATE TABLE songs(id SERIAL PRIMARY KEY, name TEXT NOT NULL, artist TEXT, album TEXT, time TEXT, is_favorite BOOLEAN);
+CREATE TABLE songs(id uuid DEFAULT gen_random_uuid() PRIMARY KEY, name TEXT NOT NULL, artist TEXT, album TEXT, time TEXT, is_favorite BOOLEAN);
