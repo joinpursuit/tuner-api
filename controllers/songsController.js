66d24a499a19e118fc19555afb36082cfb70b85d
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-02 11:01:58 -0500

    Added the delete routing

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -2,7 +2,12 @@
 const express = require("express");
 //want to use router method to list as starting path assists with routing
 const songs = express.Router();
-const { getAllSongs, getASong, addASong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getASong,
+  addASong,
+  deleteASong,
+} = require("../queries/songs");
 
 songs.get("/", async (request, response) => {
   const songs = await getAllSongs();
@@ -19,6 +24,15 @@ songs.post("/", async (request, response) => {
   console.log(request.body);
   const newSong = await addASong(request.body);
   response.status(200).json(newSong);
+});
+
+songs.delete("/:id", async (request, response) => {
+  const deletedSong = await deleteASong(request.params.id);
+  if (deletedSong.id) {
+    response.status(200).json(deletedSong);
+  } else {
+    response.status(404).json({ error: "Page Does Not Exist" });
+  }
 });
 
 module.exports = songs;
