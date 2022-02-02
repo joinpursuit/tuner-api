4c66aebef8713faae4872018baec2b0d61c2367b
BennyZ3 <bennyz3@hotmail.com>
2022-02-02 12:58:55 -0500

    finished basic requests

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,19 +1,65 @@
+const { request, response } = require("express");
 const express = require("express");
+const { json } = require("express/lib/response");
 const songs = express.Router();
-const { getAllSongs, addNewSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  addNewSong,
+  getSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
 
+//display all songs
 songs.get("/", async (req, res) => {
   console.log("Get Request");
   const songs = await getAllSongs();
-  console.log(songs);
-  res.status(200).json(songs);
+  if (songs) {
+    res.status(200).json(songs);
+  } else {
+    response.status(404).json("Song not found");
+  }
 });
 
-songs.post("/new", async (req, res) => {
-  const newSong = req.body;
+//display specific song
+songs.get("/:id", async (request, response) => {
+  console.log(`GET request for id ${request.params.id}`);
+  const song = await getSong(request.params.id);
+  if (song) {
+    response.status(200).json(song);
+  } else {
+    response.status(404).json("Song not found");
+  }
+});
+
+//delete song
+songs.delete("/:id", async (request, response) => {
+  console.log(`DELETE request for id ${request.params.id}`);
+  const song = await deleteSong(request.params.id);
+  if (song.id) {
+    response.status(200).json(song);
+  } else {
+    response.status(404).json("Song does not exist");
+  }
+});
+
+//add new song
+songs.post("/new", async (request, response) => {
+  const newSong = request.body;
   console.log(newSong);
   const addSong = await addNewSong(newSong);
-  res.status(200).json(addSong);
+  response.status(200).json(addSong);
+});
+
+//edit song
+songs.put("/:id", async (request, response) => {
+  console.log(`Edit song at ${request.params.id}`);
+  const song = updateSong(request.params.id, request.body);
+  if (song.id) {
+    response.status(200).json(song);
+  } else {
+    response.status(404).json("Song not found");
+  }
 });
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -9,9 +9,18 @@ const getAllSongs = async () => {
   }
 };
 
+const getSong = async (id) => {
+  try {
+    const song = await database.one("SELECT * FROM songs WHERE id=$1", id);
+    return song;
+  } catch (error) {
+    return error;
+  }
+};
+
 const addNewSong = async (newSong) => {
   try {
-    const songs = await database.any(
+    const song = await database.any(
       "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [
         newSong.name,
@@ -21,12 +30,41 @@ const addNewSong = async (newSong) => {
         newSong.is_favorite,
       ]
     );
-    return songs;
+    return song;
   } catch (error) {
     return error;
   }
 };
+
+const deleteSong = async (id) => {
+  try {
+    const song = await database.one(
+      "DELETE FROM songs WHERE id=$1 RETURNING *",
+      id
+    );
+    return song;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateSong = async (id, song) => {
+  const { name, artist, album, time, is_favorite } = song;
+  try {
+    const updatedSong = await database.one(
+      "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURING *",
+      [name, artist, album, time, is_favorite, id]
+    );
+    return updatedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
   getAllSongs,
   addNewSong,
+  getSong,
+  deleteSong,
+  updateSong,
 };
