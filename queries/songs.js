e615d613dff22322743bbb90b459af682ff6b3c0
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-02 21:08:29 -0500

    added update query

--- queries/songs.js
+++ queries/songs.js
@@ -43,4 +43,17 @@ const deleteSong = async (id) => {
     
 }
 
-module.exports = { getAllSongs , createSong, getSong, deleteSong };
+const updateSong = async (id, song) => {
+    try{
+        const {name, artist, album, time, is_favorite} = song;
+        const changedSong = await db.one(
+            "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURNING *",
+            [name, artist, album, time, is_favorite, id]
+        );
+        return changedSong;
+    } catch (error) {
+        return error
+    }
+}
+
+module.exports = { getAllSongs , createSong, getSong, deleteSong , updateSong};
