825144ea1b58c5bcfc12ab749f99503f869b4874
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-02 20:45:27 -0500

    added 'artist' to the createSong query

--- queries/songs.js
+++ queries/songs.js
@@ -26,8 +26,8 @@ const getSong = async (id) => {
 const createSong = async (song) => {
     try {
         const newSong = await db.one(
-            "INSERT INTO songs (name, album, time, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
-            [song.name, song.album, song.time, song.is_favorite]
+            "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [song.name, song.artist, song.album, song.time, song.is_favorite]
         );
         return newSong;
     } catch (error) {
