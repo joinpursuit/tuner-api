015c354f53a60cf0b8037ad1c7cc5702c70ac112
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-02 21:24:40 -0500

    added routes for delete/update

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const songs = express.Router();
-const { getAllSongs , createSong , getSong} = require('../queries/songs')
+const { getAllSongs , createSong , getSong, deleteSong, updateSong } = require('../queries/songs')
 
 //INDEX
 songs.get('/', async (_, res) => {
@@ -35,3 +35,28 @@ songs.get('/:id', async (req, res) => {
         return err;
     }
 })
+//DESTROY
+songs.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    const song = deleteSong(id)
+
+    if(song.id) {
+        res.status(200).json(song)
+    } else {
+        res.status(404).json({error: "Song not found"});
+    }
+})
+//UPDATE
+songs.put("/:id", async (req, res) => {
+    const updatedSong = await updateSong(req.params.id, req.body);
+      // If there is an error and object is returned which is truthy. Therefore we used .id to check that the updatedSong variable holds the keys and volume we want. 
+
+    // since we used the "one" sql method, which returns an object, if the call to the database is successful we can use dot notation in our conditional for error handling
+    if(updatedSong.id) {
+        res.status(200).json(updatedSong);
+    } else {
+        res.status(404).json({error: "Song does not exist"})
+    }
+})
+
+module.exports = songs;
