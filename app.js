5d3f79e986b92292dc7acd92d25854ec13b1e38b
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-02 19:29:35 -0500

    added songs route

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+//Dependencies
 const express = require('express');
 const cors = require('cors')
+
 const app = express();
 
+//Middleware
 app.use(cors())
 app.use(express.json())
 
+//Controller
+const songsController = require('./controllers/songsController')
+app.use('/songs', songsController)
+
+//Routes
 app.get("/", (_, response) => {
     response.status(200).send('Welcome to Tuner App!')
 })
