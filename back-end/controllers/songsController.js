e82da06f7ff1ce3b786e2e0bfeec51b5199fccce
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-02-03 16:59:54 -0500

    added full Crud functionality

--- back-end/controllers/songsController.js
+++ back-end/controllers/songsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js");
+const { getAllSongs, getSongs, createSong, deleteSong, updateSongs } = require("../queries/songs.js");
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
@@ -9,6 +9,55 @@ songs.get("/", async (req, res) => {
     } else {
         res.status(500).json({ error: "server error" });
     }
-})
+});
+
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        const allSongs = await getSongs(id);
+        if(allSongs.id){
+            res.status(200).json(allSongs);
+        } else {
+            res.status(500).json({error: "song not found"})
+        }
+    } catch(err){
+        console.log(err)
+    }
+});
+
+songs.post("/", async (req, res) => {
+    const { body } = req;
+    try{
+        const createdSong = await createSong(body)
+        if(createdSong.id){
+            res.status(200).json(createdSong);
+        } else {
+            res.status(500).json({error: "Song not found"})
+        }
+    }catch(err){
+        console.log(err);
+    } 
+});
+
+songs.delete("/:id", async(req, res) => {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id);
+    if(deletedSong.id){
+        res.status(200).json(deletedSong);
+    } else {
+        res.status(404).json({ error: "Song not found"});
+    }
+});
+
+songs.put("/:id", async(req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    const updatedSong = await updateSongs(id);
+    if(updatedSong.id){
+        res.status(200).json(updatedSong);
+    } else {
+        res.status(404).json({error: "Song not found"})
+    }
+});
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -1,14 +1,65 @@
 const db = require("../db/dbConfig.js");
 
-const getAllSongs = async ()=>{
+const getAllSongs = async () => {
     try{
         const allSongs = await db.any("SELECT * FROM songs;");
-        return allSongs;
+            return allSongs;
+        } catch(err){
+            return err;
+    }
+}
+
+const getSongs = async (id) => {
+    try{
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+            return oneSong;
+        } catch(err){
+            return err;
+    }
+}
+
+const createSong = async(song) => {
+    try{
+        const newSong = await db.one(
+            "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [song.name, song.artist, song.album, song.time, song.is_favorite]
+        )
+            return newSong;
+        } catch(err){
+            return err;
+    }
+}
+
+const deleteSong = async(id) => {
+    try{
+        const deletedSong = await db.one(
+            "DELETE FROM songs WHERE ID = $1 RETURNING *",
+            id
+        );
+            return deletedSong;
+        } catch(err){
+            return err;
+    }
+}
+
+const updateSongs = async(id, song) => {
+    try{
+        const updatedSongs = await db.one(
+            "UPDATE songs SET name = $1, artist = $2, album = $3, time = $4, is_favorite = $5, WHERE id=$6 RETURNING *"
+            [song.name, song.artist, song.album, song.time, song.is_favorite]
+            );
+            return updatedSongs;
     } catch(err){
         return err;
     }
 }
 
+
+
 module.exports = {
-    getAllSongs
+    getAllSongs,
+    getSongs,
+    createSong,
+    deleteSong,
+    updateSongs
 };
