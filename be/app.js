1f75d5b368d6ea617ef07e8a2361b34a15979786
Sara D'Arce <saradarce@gmail.com>
2022-02-02 19:45:12 -0500

    index route complete

---
+++ be/app.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+const songsController = require("./controllers/songsController.js");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Your Playlist App");
+});
+
+// Songs ROUTES
+app.use("/songs", songsController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
