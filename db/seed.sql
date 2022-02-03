f99498c4fc42c7c32f098d7752a12f29e45d27db
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-02 19:19:31 -0500

    added seed data

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+\c songs_dev;
+
+INSERT INTO song (name, artist, album, time, is_favorite) 
+VALUES 
+('do you', 'neyo', 'because of you', '2008', TRUE), 
+('love', 'keyshia cole', 'the way it is', '2005', FALSE), 
+('river', 'leon bridges', 'coming home', '2015', TRUE);
