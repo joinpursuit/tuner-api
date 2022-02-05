06d953d1b864267b447ba1c0df012213f045c893
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-05 12:23:09 -0500

    modified seed.sql file

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
-('Perfect', 'Ed Sheeran', 'Perfect', '2017', TRUE),
-('Bad Habits', 'Ed Sheeran', 'Bad Habits', '2021', TRUE),
-('SHIVERS', 'Ed Sheeran', 'Shivers', '2021', TRUE),
-('Look What You Made Me Do', 'Taylor Swift', 'Reputation', '2017', TRUE ),
-('...Ready for It?', 'Taylor Swift', 'Reputation', '2017', TRUE);
+('Perfect', 'Ed Sheeran', 'Perfect', '4:23', TRUE),
+('Bad Habits', 'Ed Sheeran', 'Bad Habits', '3:51', TRUE),
+('SHIVERS', 'Ed Sheeran', 'Shivers', '3:27', TRUE),
+('Look What You Made Me Do', 'Taylor Swift', 'Reputation', '3:31', TRUE ),
+('...Ready for It?', 'Taylor Swift', 'Reputation', '3:28', TRUE);
