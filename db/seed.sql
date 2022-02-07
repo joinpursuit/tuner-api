363fe1c43e40b98bee4a919954459a79aebd8f3e
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-07 16:15:45 -0500

    updated seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,17 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
-('Go Crazy', 'Chris Brown & Young Thug', 'Slime & B', '2:56', true),
 ('Levitating', 'Dua Lipa', 'Future Nostalgia', '3:23', true),
-('abcdefu', 'GAYLE', 'abc (nicer)', '2:48', false),
+('Lonely Together', 'Avicii', 'AVICI', '3:01', false),
+('Marry Me', 'Jason Derulo', 'Talk Dirty', '3:45', true),
 ('Ghost', 'Justin Bieber', 'Justice', '2:33', true),
 ('Woman', 'Doja Cat', 'Planet Her', '2:52', false),
+('abcdefu', 'GAYLE', 'abc (nicer)', '2:48', false),
 ('Good Ones', 'Charli XCX', 'Good Ones', '2:16', false),
+('Go Crazy', 'Chris Brown & Young Thug', 'Slime & B', '2:56', true),
 ('Heat Waves', 'Glass Animals', 'Dreamland', '3:58', true),
 ('Jealous', 'DJ Khaled', 'Father of Asahd', '4:15', true),
-('We Don''t Talk Anymore', 'Charlie Puth', 'Nine Track:', '3:37', false),
+('We Don''t Talk Anymore', 'Charlie Puth', 'Nine Track', '3:37', false),
 ('good 4 u', 'Olivia Rodrigo', 'SOUR', '2:58', false),
 ('Say Yeah', 'Wiz Khalifa', 'Say Yeah - Single', '4:02', true),
 ('Beautiful', 'Akon', 'Freedom', '5:13', false),
@@ -21,8 +23,6 @@ INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Message In A Bottle (Taylor''s Version)', 'Taylor Swift', 'Red (Taylors Version)', '3:45', true),
 ('Iffy', 'Chris Brown', 'Iffy - Single', '2:53', true),
 ('bad guy', 'Billie Eilish', 'When We All Fall Asleep, Where Do We Go?', '3:14', true),
-('Lonely Together', 'Avicii', 'AVICI', '3:01', false),
-('Marry Me', 'Jason Derulo', 'Talk Dirty', '3:45', true),
 ('Memories', 'David Guetta', 'One Love', '3:30', true),
 ('Let''s Go', 'Calvin Harris', '18 Months', '3:52', false),
 ('Better', 'Khalid', 'Suncity', '3:50', true);
