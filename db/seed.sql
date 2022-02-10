13845a910ec5860f8bbcc71f01a91b4609e890fd
vanessaconti <vconti901@aol.com>
2022-02-09 20:51:10 -0500

    changed time was the year released now is length of song

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 \c songs_dev
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
-('Waste a Moment', 'Kings of Leon', 'Walls', '2002', false),
-('Take Me to Church', 'Hozier', 'Hozier', '2013', true),
-('Budapest', 'George Ezra', 'Wanted on Voyage', '2014', true),
-('Here Comes The Sun', 'Beatles', 'Abbey Road', '1969', true),
-('Creep', 'Radiohead', 'Pablo Honey', '1993', false),
-('Show Me How to Live', 'Audioslave', 'Audioslave', '2002', false );
+('Waste a Moment', 'Kings of Leon', 'Walls', '3:00', false),
+('Take Me to Church', 'Hozier', 'Hozier', '2:30', true),
+('Budapest', 'George Ezra', 'Wanted on Voyage', '2:45', true),
+('Here Comes The Sun', 'Beatles', 'Abbey Road', '2:20', true),
+('Creep', 'Radiohead', 'Pablo Honey', '2:56', false),
+('Show Me How to Live', 'Audioslave', 'Audioslave', '2:15', false );
