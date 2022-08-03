f5e90555608340359b072d82a875e7eab1c08d13
Charlie Moran <Carlosamoran18@gmail.com>
2022-08-03 10:17:28 -0400

    update

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const songController = require("./controllers/songController.js");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Welcome to Tuner");
 });
 
 app.use("/songs", songController)
+
 app.get("*", (req, res) => {
     res.status(404).send("Page not found");
 });
