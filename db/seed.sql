5f3de4480971b1b6b19f83d2dd7c3acccc9b3912
EmaleeSoto <emaleesoto@gmail.com>
2022-08-03 14:06:06 -0400

    Fixed seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 \c songs_dev
 
 INSERT INTO song (name, artist, album, time, is_favorite) VALUES
-("Thinkin Bout You", "Frank Ocean", "Channel Orange", 4, true);
+('Thinkin Bout You', 'Frank Ocean', 'Channel Orange', 4, true);
