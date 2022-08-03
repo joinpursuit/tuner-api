8113dc69b018f52f92c6c1d2b99236405e520f6f
EmaleeSoto <emaleesoto@gmail.com>
2022-08-03 17:06:19 -0400

    Added POST

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("../db/dbConfig");
 const songs = express.Router();
 // const db = require("../db/dbConfig.js");
 const { getAllSongs, getASong } = require("../queries/songs");
@@ -14,7 +15,7 @@ songs.get("/", async (req, res) => {
   }
 });
 
-//INDIVIDUAL
+// INDIVIDUAL
 songs.get("/:id", async (req, res) => {
   const { id } = req.params;
   const song = await getASong(id);
@@ -23,6 +24,16 @@ songs.get("/:id", async (req, res) => {
   } else {
     res.status(404).send(`No such song with id of ${id}`);
   }
+});
+
+// POST
+songs.post("/new", async (req, res) => {
+  const { name, artist, album, time, is_favorite } = req.body;
+  const newSong = await db.any(
+    "INSERT INTO song (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [name, artist, album, time, is_favorite]
+  );
+  res.status(200).json({ success: true, payload: newSong });
 });
 
 module.exports = songs;
