d6d62f2b4de56b6a7afd5bc3bf6c9ad0696b33d0
Jonathan Scheiber <jon@scheiber.me>
2022-08-08 02:24:25 -0400

    update database config

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+\c tuner;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+  ('invisible string', 'Taylor Swift', 'folklore', '4:12', false),
+  ('Never Meant', 'American Football', 'American Football', '4:28', true),
+  ('Night Channels', 'Foxing', 'Dealer', '4:16', false),
+  ('Demi Moore', 'Phoebe Bridgers', 'Stranger in the Alps', '3:18', false),
+  ('Emerald Star', 'Lord Huron', 'Vide Noir', '4:38', true),
+  ('Black Sun', 'Death Cab for Cutie', 'Kintsugi', '4:49', false),
+  ('Run Away with Me', 'Carly Rae Jepsen', 'Emotion', '4:11', true),
+  ('Saturday', 'Christie Front Drive', 'Christie Front Drive', '6:35', false);
