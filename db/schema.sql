c7afc6c34ecee6d1316e80ee1d70ad415c5b0271
JC-MT <jancarlosmatias@gmail.com>
2022-08-10 17:29:25 -0400

    changes data type of schema.sql and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE songs (
     name TEXT NOT NULL,
     artist TEXT,
     album TEXT,
-    time INT,
+    time TEXT,
     is_favorite BOOLEAN
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 \c songs_dev;
 
 INSERT INTO songs (id, name, artist, album, time, is_favorite) VALUES
-( 1, 'Jireh', 'Maverick City', 'Old Church Basement' , 9, true),
-( 2, 'If I Ever Feel Better', 'Phoenix', 'United', 4, true),
-( 3, 'Shiki No Uta', 'Sapphire', 'Shiki No Uta', 4, true);
+( 1, 'Jireh', 'Maverick City', 'Old Church Basement' , '9:05', true),
+( 2, 'If I Ever Feel Better', 'Phoenix', 'United', '4:32', true),
+( 3, 'Shiki No Uta', 'Sapphire', 'Shiki No Uta', '4:13', true);
