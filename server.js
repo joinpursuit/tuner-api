1eca3fe46448b1349d56f8485eb64f19a2df38f1
Jonathan Scheiber <jon@scheiber.me>
2022-08-12 00:52:16 -0400

    correct port logic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./App");
 
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
